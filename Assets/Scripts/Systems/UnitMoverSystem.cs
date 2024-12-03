21144c66b6816980fa1a7a7a8161974cba169b62
apollyonZex <zjc.002@qq.com>
2024-12-04 01:29:08 +0800

    --Upd 128 1. 基于物理引擎的移动。

--- Assets/Scripts/Systems/UnitMoverSystem.cs
+++ Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -1,6 +1,7 @@
 using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Physics;
 using Unity.Transforms;
 
 partial struct UnitMoverSystem : ISystem
@@ -8,13 +9,19 @@ partial struct UnitMoverSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach (var (localTransform, moveSpeed) in SystemAPI.Query<RefRW<LocalTransform>, RefRO<MoveSpeed>>())
+        foreach (var (localTransform, moveSpeed, physicsVelocity) in SystemAPI.Query<RefRW<LocalTransform>, RefRO<MoveSpeed>, RefRW<PhysicsVelocity>>())
         {
             float3 targetPosition = localTransform.ValueRO.Position + new float3(1,0,0);
             float3 moveDirection = math.normalize(targetPosition - localTransform.ValueRO.Position);
 
             localTransform.ValueRW.Rotation = quaternion.LookRotation(moveDirection, math.up());
-            localTransform.ValueRW.Position = localTransform.ValueRO.Position + moveDirection * moveSpeed.ValueRO.value * SystemAPI.Time.DeltaTime;
+
+            physicsVelocity.ValueRW.Linear = moveDirection * moveSpeed.ValueRO.value;
+            physicsVelocity.ValueRW.Angular = float3.zero;
+            
+            
+            
+            //localTransform.ValueRW.Position = localTransform.ValueRO.Position + moveDirection * moveSpeed.ValueRO.value * SystemAPI.Time.DeltaTime;
         }
 
     }
