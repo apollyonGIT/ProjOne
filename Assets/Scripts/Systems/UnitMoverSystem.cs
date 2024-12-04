8a4b0fe0768c77c6952545e5b5ded764402ce74e
zhangjunchi <zhangjunchi@conchship.net>
2024-12-04 17:10:27 +0800

    --Add 1710 1. 平滑枪口转动。

--- Assets/Scripts/Systems/UnitMoverSystem.cs
+++ Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -14,7 +14,8 @@ partial struct UnitMoverSystem : ISystem
             float3 targetPosition = MouseWorldPosition.instance.getPosition();
             float3 moveDirection = math.normalize(targetPosition - localTransform.ValueRO.Position);
 
-            localTransform.ValueRW.Rotation = quaternion.LookRotation(moveDirection, math.up());
+            var rotateSpeed = 10f;
+            localTransform.ValueRW.Rotation = math.slerp(localTransform.ValueRO.Rotation, quaternion.LookRotation(moveDirection, math.up()), SystemAPI.Time.DeltaTime * rotateSpeed);
 
             physicsVelocity.ValueRW.Linear = moveDirection * moveSpeed.ValueRO.value;
             physicsVelocity.ValueRW.Angular = float3.zero;
